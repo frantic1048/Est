faf5ee0caed47d9a863175ec856532207f13725e
frantic1048 <archer@frantic1048.com>
2017-06-06 04:03:05 +0800

    test(render): Emphasis

---
+++ test/render.Emphasis.js
@@ -0,0 +1,26 @@
+import test from 'ava'
+
+import est from '../'
+
+const render = est.render
+const T = est.tokenTypes
+
+test('single line', t => {
+  const ast = {
+    T: T.Document,
+    C: [{
+      T: T.Paragraph,
+      C: [{
+        T: T.Emphasis,
+        C: [{
+          T: T.Text,
+          A: {value: 'I am Emphasis'}
+        }]
+      }]
+    }]
+  }
+  const expected = '<p><em>I am Emphasis</em></p>'
+  const actual = render(ast)
+  t.is(actual, expected,
+    'should render a simple emphasis')
+})
